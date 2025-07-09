86c68e7eee1fbd001de0e676c2d7f738901866f9
prithviraj m pillai <prithvi_mp@yahoo.in>
2025-07-09 11:31:17 +0000

    adding github worflows

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print(f"5 + 3 = {add(5, 3)}")
+    print(f"5 - 3 = {subtract(5, 3)}")
+
+
 @app.route('/')
 def home():
     return "Hello from Jenkins Python Pipeline!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6001)
